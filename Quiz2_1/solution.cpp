38d2f211210cbafef452293a95aa8da7c0908658
Cuda Chen <clh960524@gmail.com>
2021-01-01 18:58:14 +0800

    add C++ solution of Quiz #2.1

--- Quiz2_1/solution.cpp
+++ Quiz2_1/solution.cpp
@@ -1,17 +1,20 @@
+// DP
+// house robber variant
+
 #include <cstdio>
 #include <algorithm>
 using namespace std;
 
-long n, a[2 * 10^6], dp[2 * 10^6] = { 0 };
+long n, a[2000000], dp[2000000] = { 0 };
 
 int main() {
-    scanf("%d", &n);
+    scanf("%ld", &n);
     for(int i = 0; i < n; i++)
-        scanf("%d", &a[i]);
+        scanf("%ld", &a[i]);
     for(int i = 0; i < n; i++) {
         dp[i] = max((i > 1 ? dp[i - 2] : 0) + a[i],
                     (i > 0 ? dp[i - 1] : 0));
     }
-    printf("%d\n", dp[n - 1]);
+    printf("%ld\n", dp[n - 1]);
     return 0;
 }
