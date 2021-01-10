c676ce594f9568bf091409d6e058241ddcfbd637
Cuda Chen <clh960524@gmail.com>
2021-01-10 17:18:59 +0800

    add C++ AC solution of Quiz #2.2

--- Quiz2_2/main.cpp
+++ Quiz2_2/main.cpp
@@ -9,13 +9,22 @@ int main() {
     scanf("%d %d", &n, &m);
     for(int i = 0; i < n; i++)
         scanf("%d", &a[i]);
+
+    vector<int> table = vector<int>(n, 0);
     for(int i = 0; i < m; i++) {
         int l, r;
         scanf("%d %d", &l, &r);
-        vector<int> table = vector<int>(n, 0);
         for(int j = l; j < r; j++)
             table[a[j]]++;
-        printf("%ld\n", max_element(table.begin(), table.end()) - table.begin());
+
+        // https://github.com/algo-seacow/NCTU-ItoA-2020-Fall/tree/master/1134
+        pair<int, int> result; // value & the number of the value occurs
+        for(int j = l; j < r; j++) {
+            if((table[a[j]] > result.second) || (a[j] < result.first && table[a[j]] == result.second))
+                result = {a[j], table[a[j]]};
+            table[a[j]] = 0;
+        }
+        printf("%d\n", result.first);
     }
 
     return 0;
